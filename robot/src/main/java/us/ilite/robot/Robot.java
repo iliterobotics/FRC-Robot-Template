93a753ac9273274de0e6fc6ff91cedfbbb3e33b8
John Anselmo <jjohnanselmo@gmail.com>
2019-11-07 19:40:31 -0500

    robot structure

---
+++ robot/src/main/java/us/ilite/robot/Robot.java
@@ -0,0 +1,55 @@
+package us.ilite.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+    @Override
+    public void RobotInit() {
+
+    }
+
+    @Override
+    public void robotPeriodic() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void disabledInit() {
+
+    }
+
+    @Override
+    public void disabledPeriodic() {
+
+    }
+
+    @Override
+    public void testInit() {
+
+    }
+
+    @Override
+    public void testPeriodic() {
+
+    }
+}
